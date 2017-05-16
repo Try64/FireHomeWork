fe8da7e6fda98148b0f2ba33e82c9e15f402b830
Try64 <bubt@outlook.in>
2017-05-16 23:32:40 +0600

    InitialUpload
    Just uploaded the first commit and this project is one of the Learning
    iOS and Firebase projects

---
+++ FireHomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireHomeWork
+//
+//  Created by studentNumber13 on 5/16/17.
+//  Copyright © 2017 studentNumber13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FireHomeWork/Pods-FireHomeWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireHomeWork : NSObject
+@end
+@implementation PodsDummy_Pods_FireHomeWork
+@end
