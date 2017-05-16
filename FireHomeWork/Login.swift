c3d46a2e820e6b132a4be5bfddfb9497d84dc762
Try64 <bubt@outlook.in>
2017-05-16 23:52:11 +0600

    TemporaryCommit
    Unplanned Commit , just uploading some new files

---
+++ FireHomeWork/Login.swift
@@ -0,0 +1,36 @@
+//
+//  Login.swift
+//  FireHomeWork
+//
+//  Created by studentNumber13 on 5/16/17.
+//  Copyright © 2017 studentNumber13. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FireHomeWork/SignUp.swift
@@ -0,0 +1,36 @@
+//
+//  SignUp.swift
+//  
+//
+//  Created by studentNumber13 on 5/16/17.
+//
+//
+
+import UIKit
+import Firebase
+
+class SignUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
